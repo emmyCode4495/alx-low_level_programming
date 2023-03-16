094d3ae39ee13b97466444189706107c45471097
emmyCode4495 <emmycode4495@gmail.com>
2023-03-16 16:40:39 -0700

    Hello Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *main - this i sthe entry point of every c program
+ *Return: The return value after the program compiles
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
