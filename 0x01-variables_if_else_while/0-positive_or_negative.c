03cad65c6280bfc504894959c537c0a8c0180b6e
emmyCode4495 <emmycode4495@gmail.com>
2023-03-20 08:00:27 -0700

    C program to show state of a printed random number

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ *main - The entry point of every C program
+ *Return: the integre to be returned after program run successfully
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+
+	{
+		printf("%d is positive", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero", n);
+	}
+	else
+	{
+		printf("%d is negative", n);
+	}
+	return (0);
+}
