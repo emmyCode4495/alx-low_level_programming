904cef046df41f815d6a70e298ae723d94e1506c
emmyCode4495 <emmycode4495@gmail.com>
2023-03-20 10:24:12 -0700

    Modified the file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - The entry point of the C program
+ *Return: the integer to be returned after succesful runtime
+ *Description: C program to print out alphabets in both upper and lower
+ *
+ */
+int main(void)
+{
+	int lowerCaseStartValue = 97;
+	int upperCaseStartValue = 65;
+
+	while (lowerCaseStartValue <= 122)
+	{
+		putchar(lowerCaseStartValue);
+	}
+	while (upperCaseStartValue <= 90)
+	{
+		putchar(upperCaseStartValue);
+	}
+	putchar('\n');
+	return (0);
+}
