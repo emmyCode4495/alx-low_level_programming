2e0533f19ed6dc1a4977bcde6fb0f856029489a2
emmyCode4495 <emmycode4495@gmail.com>
2023-03-20 16:17:29 -0700

    Printing numbers separated ny comma

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,22 +2,19 @@
 /**
  *main - themain entry point
  *Return: the integer to be returned after successful compilATION
- *dESCRIPTION: c PROGRAM TO PRINT BASE 16 NUMBERS
+ *dESCRIPTION: c PROGRAM TO PRINt NUMBERS with separation character
  *
  */
 int main(void)
 {
 	int numbers;
-	int alphabets;
 
 	for (numbers = 48; numbers <= 57; numbers++)
 	{
 		putchar(numbers);
+		putchar(',');
+		putchar(' ');
 	}
-	for (alphabets = 97; alphabets <= 102; alphabets++)
-	{
-		putchar(alphabets);
-	}
-	putchar('\n);
+	putchar('\n');
 	return (0);
 }
