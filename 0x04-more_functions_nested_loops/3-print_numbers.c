6773f63b4f3351a081f8ff471684e9fb6b1b776d
emmyCode4495 <emmycode4495@gmail.com>
2023-03-23 06:07:23 -0700

    Function to print numbers from 0 - 9 with a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers - a void function that prints numbers
+ *Return: the return type is void
+ *
+ *
+ */
+
+void print_numbers(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 0; numbers++)
+	{
+		_putchar(numbers + '0');
+	}
+	_putchar('\n');
+}
