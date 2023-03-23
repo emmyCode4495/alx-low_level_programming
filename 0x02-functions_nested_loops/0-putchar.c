11477b9af30757b2a0069f75593f799bfb49df43
emmyCode4495 <emmycode4495@gmail.com>
2023-03-22 20:00:43 -0700

    Printing the putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *main-the entry point of the program
+ *Return: The return value
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
