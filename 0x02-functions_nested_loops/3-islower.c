1f52922be1b120a89c94922bedd33d1be2f3c84a
emmyCode4495 <emmycode4495@gmail.com>
2023-03-22 21:53:13 -0700

    modified the file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *_islower - Fornlower case print out
+ *lower.c*_islower - Fornlower case print out
  *@c: The integer variable
  *Return: It returns integer
  *
@@ -10,7 +10,7 @@
 
 int _islower(int c)
 {
-	if (c <= 97 && c >= 122)
+	if (c >= 97 && c <= 122)
 	{
 		return (1);
 	}
