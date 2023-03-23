8084cb7354c9bf14e401c95a438c5f3de8cf91e1
emmyCode4495 <emmycode4495@gmail.com>
2023-03-23 16:24:15 -0700

    Printing numbers several times task

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ *more_numbers - The prototype
+ *Return: void
+ *
+ *
+ *
+ */
+
+void more_numbers(void)
+{
+	int num1, num2;
+
+	for (num1 = 0; num1 <= 10; num1++)
+	{
+		for (num2 = 0; num2 <= 14; num2++)
+		{
+			if (y > 9)
+			{
+				_putchar((num2 / 10) + '0');
+			}
+			_putchar((num2 % 10) + '0');
+		}
+		_putchar('\n');
+
+	}
+}
+}
