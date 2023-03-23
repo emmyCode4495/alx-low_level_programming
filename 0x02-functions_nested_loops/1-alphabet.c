7b742a877d28b880905c65c7d15dd2fd27f58f5f
emmyCode4495 <emmycode4495@gmail.com>
2023-03-22 20:34:21 -0700

    Printing alphabets a- z

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - a prototype
+ *C progran to print all alphabeta
+ *Return: void
+ */
+void print_alphabet(void)
+{
+	char alphabets;
+	
+	for (alphabets = 'a'; alphabets <= 'z'; alphabets++)
+	{
+		_putchar(alphabets);
+	}
+	_putchar('\n');
+}
