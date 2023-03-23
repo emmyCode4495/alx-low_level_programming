21a0c601b37838773f5279e54f61827b90746234
emmyCode4495 <emmycode4495@gmail.com>
2023-03-23 05:56:01 -0700

    Function to multiply 2 numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *mul - function to multiply 2 numbers
+ *@x: The first variable to be returned
+ *@y: the scond variable to be returned
+ *Return: the final result to be returned after compilation
+ */
+int mul(int x, int y)
+{
+	return (x * y);
+}
