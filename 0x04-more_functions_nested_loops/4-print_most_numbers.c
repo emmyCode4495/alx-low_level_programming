64c8ad7d26995ce34cd671b7e0493a73391b5519
emmyCode4495 <emmycode4495@gmail.com>
2023-03-23 15:41:15 -0700

    print numbers with exception

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - void function to print numbers
+ *Return : the return typeis void
+ *
+ *
+ *
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int num = 0;
+
+	for (; num <= 9; num++)
+	{
+		if (num == 2 || num == 4)
+		{
+			continue;
+		}
+		els
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
+}
