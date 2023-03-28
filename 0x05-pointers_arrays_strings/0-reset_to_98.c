08064f9bd83842dab6f2eefa041e51eb2bd40518
emmyCode4495 <emmycode4495@gmail.com>
2023-03-28 14:12:46 -0700

    Getting int as pointer and print value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ *reset_to_98 -takes the pointer to an int and updates th e memory
+ *@n: The pointer vsriable with type INT
+ *Return: void
+ *
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
