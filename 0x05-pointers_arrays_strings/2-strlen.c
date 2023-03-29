5aad6b7c482dcb894c6f1a8ce58c04abf3bf2c43
emmyCode4495 <emmycode4495@gmail.com>
2023-03-28 17:56:04 -0700

    The report by itd length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *_strlen - the entry point to the program
+ *@str: The varaiable that stores dat
+ *Return: Void
+ *
+ */
+
+size_t _strlen(const char *str)
+{
+	size_t length = 0;
+
+	while(*str++)
+	{
+		length++;
+	}
+	return (length);
+}
