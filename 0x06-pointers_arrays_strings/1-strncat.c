50d610cfcf7c7bad830ebaa40d6ed63964b3a05c
emmyCode4495 <emmycode4495@gmail.com>
2023-03-31 14:57:23 -0700

    Made modifications to the code

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,11 +13,20 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index = 0, dest_len = 0;
+	int iIndex = 0;
+	int jIndex = 0;
 
-	while (dest[index++])
-		dest_len++;
-	for (index = 0; src[index] && index < n; index++)
-		dest[dest_len++] = src[index];
-	return(dest);
+	while (dest[iIndex++] != '\0')
+	{
+		iIndex++;
+	}
+
+	while (jIndex < n && src[jIndex] != '\0')
+	{
+		dest[iIndex] = src[jIndex];
+		iIndex++;
+		jIndex++;
+	}
+	dest[iIndex] = '\0';
+	return (dest);
 }
