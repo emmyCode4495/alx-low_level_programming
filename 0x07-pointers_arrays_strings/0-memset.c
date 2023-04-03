ad5f3d540220fe67c0f7b777f5750ccf8810f466
emmyCode4495 <emmycode4495@gmail.com>
2023-04-03 07:16:34 -0700

    The ALX memset project

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_memset - a function that fills a block of memory with a given value
+ *@s: The is the starting point of the address
+ *@b: The value to be gotten from the user
+ *@n: the required number of bytes to be changed
+ *
+ *
+ *
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int iIndex = 0;
+
+	/* Using for loop to carry out the filling of the memory */
+	for (; n > 0; iIndex++)
+	{
+		s[iIndex] = b;
+		n--;
+	}
+	return (s);
+
+}
