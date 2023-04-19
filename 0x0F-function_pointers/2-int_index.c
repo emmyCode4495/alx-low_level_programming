276c99f1d9f94de4456ec6b005f0b2877051e4b7
emmyCode4495 <emmycode4495@gmail.com>
2023-04-19 06:18:40 -0700

    2-int_index task

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - return index acording to boolean command
+ * @array: array of pointers
+ *@size: size of te array of pointers
+ * @cmp: pointer to function to carry out the comman
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
